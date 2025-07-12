b9216b4847fc03b47881074019cdf8dbd1dc788f
tera8m4 <82479019+tera8m4@users.noreply.github.com>
2025-07-12 20:50:19 +0300

    handle key presses

--- server/src/main.rs
+++ server/src/main.rs
@@ -10,18 +10,35 @@ use flatbuffers::FlatBufferBuilder;
 use tokio::net::TcpListener;
 use tokio::sync::mpsc;
 use futures_util::{StreamExt, SinkExt};
+use global_hotkey::{
+    hotkey::{Code, HotKey, Modifiers},
+    GlobalHotKeyManager, GlobalHotKeyEvent,
+};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 #[allow(dead_code, unused_imports, unsafe_code, unsafe_op_in_unsafe_fn)]
 mod messages_generated;
 use messages_generated::protocol::*;
 
-async fn websocket_handler(ws: WebSocketUpgrade) -> response::Response {
-    ws.on_upgrade(handle_socket)
+type ClientSenders = Arc<Mutex<Vec<mpsc::Sender<Vec<u8>>>>>;
+
+async fn websocket_handler(
+    ws: WebSocketUpgrade,
+    axum::extract::State(clients): axum::extract::State<ClientSenders>,
+) -> response::Response {
+    ws.on_upgrade(move |socket| handle_socket(socket, clients))
 }
 
-async fn handle_socket(socket: WebSocket) {
+async fn handle_socket(socket: WebSocket, clients: ClientSenders) {
     let (mut sender, mut receiver) = socket.split();
     let (tx, mut rx) = mpsc::channel::<Vec<u8>>(100);
+    
+    // Add this client to the global list
+    {
+        let mut clients_guard = clients.lock().await;
+        clients_guard.push(tx.clone());
+    }
     
     let socket_tx = tx.clone();
     let message_handler = tokio::spawn(async move {
@@ -52,6 +69,12 @@ async fn handle_socket(socket: WebSocket) {
     });
 
     let _ = tokio::try_join!(message_handler, response_handler);
+    
+    // Remove this client from the global list when connection closes
+    {
+        let mut clients_guard = clients.lock().await;
+        clients_guard.retain(|client_tx| !client_tx.is_closed());
+    }
 }
 
 async fn handle_flatbuffer_message(data: &[u8]) -> Option<Vec<u8>> {
@@ -80,14 +103,66 @@ async fn create_initialization_response(request_id: u64) -> Vec<u8> {
     builder.finished_data().to_vec()
 }
 
+async fn send_hotkey_message_to_clients(clients: ClientSenders) {
+    let mut builder = FlatBufferBuilder::new();
+    
+    // Create a message indicating hotkey was pressed
+    let response = Response::create(&mut builder, &ResponseArgs {
+        id: 0, // Using 0 for hotkey-triggered messages
+        data_type: ResponseData::NONE,
+        data: None,
+    });
+    
+    builder.finish(response, None);
+    let message = builder.finished_data().to_vec();
+    
+    // Send to all connected clients
+    let clients_guard = clients.lock().await;
+    for client_tx in clients_guard.iter() {
+        let _ = client_tx.send(message.clone()).await;
+    }
+    println!("Hotkey pressed - sent message to {} clients", clients_guard.len());
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(websocket_handler));
+    // Create shared state for connected clients
+    let clients: ClientSenders = Arc::new(Mutex::new(Vec::new()));
+    
+    // Set up global hotkey manager
+    let manager = GlobalHotKeyManager::new().unwrap();
+    let hotkey = HotKey::new(Some(Modifiers::CONTROL), Code::KeyA);
+    manager.register(hotkey).unwrap();
+    println!("Registered Ctrl+A hotkey");
+    
+    // Clone clients for hotkey task
+    let clients_for_hotkey = clients.clone();
+    
+    // Spawn hotkey listener task
+    let hotkey_task = tokio::spawn(async move {
+        let receiver = GlobalHotKeyEvent::receiver();
+        loop {
+            if let Ok(_event) = receiver.try_recv() {
+                send_hotkey_message_to_clients(clients_for_hotkey.clone()).await;
+            }
+            tokio::time::sleep(tokio::time::Duration::from_millis(10)).await;
+        }
+    });
+    
+    // Set up WebSocket server
+    let app = Router::new()
+        .route("/", get(websocket_handler))
+        .with_state(clients);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 49156));
     let listener = TcpListener::bind(&addr).await.unwrap();
     
     println!("WebSocket server listening on: {}", addr);
     
-    axum::serve(listener, app).await.unwrap();
+    // Run both server and hotkey listener concurrently
+    let server_task = tokio::spawn(async move {
+        axum::serve(listener, app).await.unwrap();
+    });
+    
+    let _ = tokio::try_join!(server_task, hotkey_task);
 }
